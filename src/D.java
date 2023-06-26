ce2d92d40714c62b99cd462f73d96072b1f18724
Max Nesazal <maxnesazal1492@gmail.com>
2023-06-26 14:54:57 +0200

    b6

--- src/D.java
+++ src/D.java
@@ -1,5 +1,11 @@
 public class D {
     public void future(String word1, String word2) {
         System.out.println(word1 + "," + word2);
+        System.out.println(word1 + "," + word2);
+        System.out.println(word1 + "," + word2);
+        System.out.println(word1 + "," + word2);
+        System.out.println(word1 + "," + word2);
+        System.out.println(word1 + "," + word2);
+        System.out.println(word1 + "," + word2);
     }
 }
