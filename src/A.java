0c4707d9674173ec209880786792746b2f2f9e31
Max Nesazal <maxnesazal1492@gmail.com>
2023-06-26 14:33:34 +0200

    b6

--- src/A.java
+++ src/A.java
@@ -1,5 +1,5 @@
 public class A {
-    public static void greet() {
-        System.out.println("greetings");
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 }
